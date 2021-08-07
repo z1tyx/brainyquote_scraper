1bf774e7484d3e568ee8431a0720fd1e31d2ec2d
z1tyx <zqx1ty@gmail.com>
2021-08-07 06:04:25 -0700

    Scrape quotes based on topic

--- main.py
+++ main.py
@@ -14,3 +14,23 @@ if request.status_code == 404:
 page = request.text
 
 soup = BeautifulSoup(page,'html.parser')
+
+counter = 1
+
+while True:
+    quote = soup.find('div',id='pos_1_' + str(counter))
+    if quote == None:
+        break
+
+    counter += 1
+
+    if quote.find('a',class_='oncl_q') == None:
+        continue
+
+    quote_text = quote.find_all('a',class_='oncl_q')
+    quote_text = "".join(quote_text[len(quote_text) - 1].text.split("\n"))
+    author = "".join(quote.find('a',class_='oncl_a').text.split("\n"))
+
+    print("\"" + quote_text + "\"")
+    print("- " + author)
+    print()
