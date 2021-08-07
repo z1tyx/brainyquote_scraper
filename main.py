3e900a114da52984764e168710316abe6154dec7
z1tyx <zqx1ty@gmail.com>
2021-08-07 06:09:30 -0700

    Output the quotes into the txt file format

--- main.py
+++ main.py
@@ -17,20 +17,20 @@ soup = BeautifulSoup(page,'html.parser')
 
 counter = 1
 
-while True:
-    quote = soup.find('div',id='pos_1_' + str(counter))
-    if quote == None:
-        break
+with open("quotes.txt","w") as quotes_file:
+    while True:
+        quote = soup.find('div',id='pos_1_' + str(counter))
+        if quote == None:
+            break
 
-    counter += 1
+        counter += 1
 
-    if quote.find('a',class_='oncl_q') == None:
-        continue
+        if quote.find('a',class_='oncl_q') == None:
+            continue
 
-    quote_text = quote.find_all('a',class_='oncl_q')
-    quote_text = "".join(quote_text[len(quote_text) - 1].text.split("\n"))
-    author = "".join(quote.find('a',class_='oncl_a').text.split("\n"))
+        quote_text = quote.find_all('a',class_='oncl_q')
+        quote_text = "".join(quote_text[len(quote_text) - 1].text.split("\n"))
+        author = "".join(quote.find('a',class_='oncl_a').text.split("\n"))
 
-    print("\"" + quote_text + "\"")
-    print("- " + author)
-    print()
+        quotes_file.write("\"" + quote_text + "\"" + "\n")
+        quotes_file.write("- " + author + "\n\n")
